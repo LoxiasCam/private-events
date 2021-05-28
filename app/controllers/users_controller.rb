4215ff22999dcfcda8ba72276c7c7f6fe098a492
marysol <marilirulita@gmail.com>
2021-05-28 15:31:09 -0700

    Add events links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,3 @@
 class UsersController < ApplicationController
-
+  @user = User.find(params[:id])
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "events#index"
   resources :users, only: [:show]
-  resources :events, only: [:new, :create]
+  resources :events, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
