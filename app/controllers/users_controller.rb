8411e06138a9f35f5fc14bf3b47b345a99399d35
marysol <marilirulita@gmail.com>
2021-06-05 06:14:17 -0700

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-  
-  private
-
-  def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
-
-  def acount_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  def show
   end
 end
