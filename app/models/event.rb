f3bf47c9df22c6d1999f13543630d310a93682db
marysol <marilirulita@gmail.com>
2021-06-08 09:40:06 -0700

    Add model destroy dependeceis

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :event_attendees, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendees, source: :event_attendee
   belongs_to :creator, class_name: "User", foreign_key: 'user_id'
 end
