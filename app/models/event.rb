171737c4d3a6b83e1bbea17b3ca7955d079e3383
marysol <marilirulita@gmail.com>
2021-06-08 11:51:21 -0700

    Add forms users style

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   has_many :event_attendees, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendees, source: :event_attendee
   belongs_to :creator, class_name: "User", foreign_key: 'user_id'
+
+  validates :event_date, presence: true
+  validates :event_name, presence: true
 end
