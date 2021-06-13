26bf8125b06f0147590b89a8534c9f4db8a38091
marysol <marilirulita@gmail.com>
2021-06-13 07:23:13 -0700

    Remove space

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,5 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-  
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
